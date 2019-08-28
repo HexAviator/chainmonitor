d2b36d2948c1c8d5f40a3730df8c1ef926f97bc7
hexaviator <hexaviatordev@gmail.com>
2019-08-28 22:53:51 +0300

    adding winner address

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 var nodeIP = "95.179.178.196"
 var nodePort = "9988"
 
-func connect() (blockNumber, blockTime, diff, hashrate, nrOfTxs, info string) {
+func connect() (blockNumber, blockTime, diff, hashrate, nrOfTxs, winner, info string) {
 	client, err := ethclient.Dial("http://" + nodeIP + ":" + nodePort)
 	if err != nil {
 		log.Fatal(err)
@@ -25,14 +25,20 @@ func connect() (blockNumber, blockTime, diff, hashrate, nrOfTxs, info string) {
 	}
 
 	block, err := client.BlockByNumber(context.Background(), header.Number)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set the vars
 	blockNumber = "Current block number    : " + header.Number.String()
 	blockTime = "Current block time      : " + strconv.Itoa(int(header.Time))
 	diff = "Difficulty              : " + header.Difficulty.String()
 	hsrate := header.Difficulty.Uint64() / uint64(14)
 	hashrate = "Hashrate                : " + strconv.Itoa(int(hsrate))
 	nrOfTxs = "Number of txs in block  : " + strconv.Itoa(block.Transactions().Len())
+	winner = "Winner address of block : " + header.Coinbase.String()
 	info = "Refresh page for new block info ..."
-	return blockNumber, blockTime, diff, hashrate, nrOfTxs, info
+	return blockNumber, blockTime, diff, hashrate, nrOfTxs, winner, info
 }
 
 func main() {
@@ -61,12 +67,20 @@ func printPage(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "<-------------------------------------------------------------------------------->")
 	fmt.Fprintln(w, "<         Connecting to node with IP : ", nodeIP, " and port : ", nodePort, "        >")
 	fmt.Fprintln(w, "<-------------------------------------------------------------------------------->")
-	bln, blt, diff, hsrate, nrOfTxs, info := connect()
+	bln, blt, diff, hsrate, nrOfTxs, winner, info := connect()
 	fmt.Fprintln(w, bln)
 	fmt.Fprintln(w, blt)
 	fmt.Fprintln(w, diff)
 	fmt.Fprintln(w, hsrate)
 	fmt.Fprintln(w, "Price                   : Not available yet")
 	fmt.Fprintln(w, nrOfTxs)
+	fmt.Fprintln(w, winner)
 	fmt.Fprintln(w, info)
+	fmt.Fprintln(w, "<-------------------------------------------------------------------------------->")
+
+}
+
+func percentChange(startValue, endValue int) int {
+	return ((endValue - startValue) / startValue) * 100
+
 }
